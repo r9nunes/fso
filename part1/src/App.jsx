058ffa5156f3dfcdc5e2bb8834a60b5c3153de1c
Ricardo Nunes <ricardo@ifal.edu.br>
2025-12-25 18:12:17 -0300

    part 1 - exercise 4

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,28 +1,29 @@
 function App() {
-  const course = 'Desenvolvimento de aplicação Half Stack - Part1 E3.'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
-  const content = [part1, part2, part3];
+  const course = 'Desenvolvimento de aplicação Half Stack - Part1 E4.'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <>
       <Header course={course} />
-      <Content content={ content }/>
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Content content={parts} />
+      <Total total={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
     </>
   )
 }
+
 
 function Content(params) {
   // console.log(params);
